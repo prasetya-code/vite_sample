61458b46c615678f60d33c7ff52190c84a9ef3cd
Prasetya <prasetya.career@gmail.com>
2026-03-20 11:12:55 +0700

    m

--- .npm/src/bootstrap.js
+++ .npm/src/bootstrap.js
@@ -2,10 +2,14 @@ import Alpine from 'alpinejs'
 import mask from '@alpinejs/mask'
 import persist from '@alpinejs/persist'
 import intersect from '@alpinejs/intersect'
+import collapse from '@alpinejs/collapse'
+import focus from '@alpinejs/focus'
 
 Alpine.plugin(mask)
 Alpine.plugin(persist)
 Alpine.plugin(intersect)
+Alpine.plugin(collapse)
+Alpine.plugin(focus)
 
 window.Alpine = Alpine
 Alpine.start()
--- .npm/vite.config.js
+++ .npm/vite.config.js
@@ -5,13 +5,16 @@ export default defineConfig({
   plugins: [
     legacy({ targets: ['defaults', 'not IE 11'] }),
   ],
+  
   build: {
     outDir: '../app/static/js',   // naik satu level ke root, lalu masuk app/static/js
     emptyOutDir: true,
+    reportCompressedSize: false,  // Matikan warning plugin timings
     rollupOptions: {
       input: {
         main: './src/main.js',    // relatif dari frontend/
       },
+
       output: {
         entryFileNames: '[name].js',
         chunkFileNames: '[name].js',
